a1baaa7bdaf6c8de6e6d0f41f69ec97f3110181c
SaviHerath <savi@chaebanicecream.com>
2021-05-25 01:35:02 -0500

    Updates

---
+++ precache-manifest.554aa2b8c8661d56d43e2ce6d0b895fe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c90462ade0380dfdea366696b25685e",
+    "url": "/Monster-Friend-Search/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/Monster-Friend-Search/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "319feaa8d408679bb574",
+    "url": "/Monster-Friend-Search/static/css/main.57e9cd1d.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/Monster-Friend-Search/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-Friend-Search/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "319feaa8d408679bb574",
+    "url": "/Monster-Friend-Search/static/js/main.1a402c60.chunk.js"
+  },
+  {
+    "revision": "df40cb67ef656b222b8b",
+    "url": "/Monster-Friend-Search/static/js/runtime-main.7a1f38c5.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Monster-Friend-Search/static/media/SEGA.12a3db8b.woff"
+  }
+]);
