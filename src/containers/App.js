b68c463addf2cf6c469503d2ea1a12763ff62eb2
SaviHerath <savi@chaebanicecream.com>
2022-02-14 19:10:12 +0530

    Changed heading

--- src/containers/App.js
+++ src/containers/App.js
@@ -32,7 +32,7 @@ class App extends Component {
       <h1>Loading</h1> :
       (
         <div className='tc'>
-          <h1 className='f1'>Friendly Monster!!</h1>
+          <h1 className='f1'>Friendly Monsters!!</h1>
           <SearchBox searchChange={this.onSearchChange}/>
           <Scroll>
             <CardList robots={filteredRobots} />
